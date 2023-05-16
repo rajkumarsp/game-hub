892169b56fa6bbff97245d5dd9d575f204e095ad
Rajkumar Pothiraj <rajkumarp@novusteq.com>
2023-05-16 22:56:26 +0530

    Responsive Layout changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main" `,
+      }}
+    >
+      <GridItem area="nav" bg="yellow.100">
+        NAV
+      </GridItem>
+      <GridItem area="main" bg="teal.100">
+        MAIN
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="pink.100">
+          ASIDE
+        </GridItem>
+      </Show>
+    </Grid>
+  );
 };
 
 export default App;
