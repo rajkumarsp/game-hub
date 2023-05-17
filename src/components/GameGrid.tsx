dc86776b283ac012254ef938487359bae73940b0
Rajkumar Pothiraj <rajkumarp@novusteq.com>
2023-05-17 23:47:58 +0530

    Skeleton component added

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,12 @@
 import { Text, SimpleGrid } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
     <>
@@ -13,6 +16,11 @@ const GameGrid = () => {
         spacing="10px"
         padding="2px"
       >
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardSkeleton key={skeleton}></GameCardSkeleton>
+          ))}
+
         {games.map((game) => (
           <GameCard key={game.id} game={game}></GameCard>
         ))}
